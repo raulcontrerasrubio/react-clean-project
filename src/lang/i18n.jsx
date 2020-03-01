079a31936853964c9788a371a2307954a6a7f7b2
Raul Contreras <raulcontrerasrubio@gmail.com>
2020-03-01 21:31:37 +0100

    Added internationalization

---
+++ src/lang/i18n.jsx
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import {initReactI18next} from 'react-i18next';
+import es from './es/index';
+import en from './en/index';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    detection: {
+      order: ['localStorage', 'cookie', 'navigator', 'querystring', 'htmlTag', 'path', 'subdomain'],
+      caches: ['localStorage', 'cookie'],
+      excludeCacheFor: ['cimode'],
+      cookieMinutes: 10,
+      cookieDomain: window.location.hostname,
+    },
+    resources: {en, es},
+    whitelist: ['en', 'es'],
+    ns: ['app'],
+    defaultNS: ['app'],
+    fallbackLng: 'en',
+    debug: false,
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,14 +1,17 @@
 import React, {useMemo} from 'react';
 import PageLoad from '../../components/Animations/PageLoad/PageLoad';
+import {useTranslation} from 'react-i18next';
 
 const MainPage = () => {
+  const {t, i18n} = useTranslation();
+
   const loadedComponent = useMemo(
     () => (
       <div>
-        <p>Main Page</p>
+        <p>{t('app:test')}</p>
       </div>
     ),
-    []
+    [t, i18n]
   );
 
   return <PageLoad onSuccessComponent={loadedComponent} />;
