acaa27b5739ba503707250886b3323b9e8057196
Raul Contreras <raulcontrerasrubio@gmail.com>
2020-03-01 22:17:24 +0100

    Added material-ui

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,8 @@
 import React, {useMemo} from 'react';
 import PageLoad from '../../components/Animations/PageLoad/PageLoad';
 import {useTranslation} from 'react-i18next';
+import {Apple} from '@material-ui/icons';
+import {AcUnit} from '@material-ui/icons';
 
 const MainPage = () => {
   const {t, i18n} = useTranslation();
@@ -8,7 +10,9 @@ const MainPage = () => {
   const loadedComponent = useMemo(
     () => (
       <div>
-        <p>{t('app:test')}</p>
+        <p onClick={() => i18n.changeLanguage('es')}>
+          {t('app:test')} <Apple /> <AcUnit />
+        </p>
       </div>
     ),
     [t, i18n]
